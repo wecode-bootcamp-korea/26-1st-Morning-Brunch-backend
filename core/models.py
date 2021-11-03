fc4bdd0d4dfab262055700519ea2d72c5d60e6b2
gshduet <modis2199@gmail.com>
2021-11-03 16:36:46 +0900

    ADD : 이력관리를 위한 core 앱 추가 및 기타 컨벤션 수정

---
+++ core/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class TimeStampModel(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
--- posts/models.py
+++ posts/models.py
@@ -1,33 +1,34 @@
-from django.db import models
+from django.db      import models
+
+from core.models    import TimeStampModel    
 
 class Magazine(models.Model):
-    name            = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
     class Meta:
-        db_table    = 'megazines'
+        db_table = 'magazines'
 
-class Post(models.Model):
+class Post(TimeStampModel):
     magazine        = models.ForeignKey('Magazine',on_delete=models.CASCADE)
     title           = models.CharField(max_length=30)
     sub_title       = models.CharField(max_length=30)
     content         = models.TextField()
-    create_time     = models.DateTimeField(auto_now_add=True)
-    update_time     = models.DateTimeField(auto_now=True)
-    delete_time     = models.DateTimeField(auto_now=True)
+    delete_time     = models.DateTimeField()
+    is_delete       = models.BooleanField(default=False)
     
     class Meta:
-        db_table    = 'posts'
+        db_table = 'posts'
 
 class CoverImage(models.Model):
-    post            = models.ForeignKey('Post',on_delete=models.CASCADE)
-    cover_image_url = models.URLField(max_length=200)
-    delete_time     = models.DateTimeField(auto_now=True)
-    
+    post        = models.ForeignKey('Post',on_delete=models.CASCADE)
+    url         = models.URLField(max_length=200)
+    is_delete   = models.BooleanField(default=False)
+
     class Meta:
-        db_table    = 'cover_images'
+        db_table = 'cover_images'
 
 class PostTag(models.Model):
-    post            = models.ForeignKey('post',on_delete=models.CASCADE)
-    tag             = models.ForeignKey('users.Tag',on_delete=models.CASCADE)
+    post = models.ForeignKey('post',on_delete=models.CASCADE)
+    tag = models.ForeignKey('users.Tag',on_delete=models.CASCADE)
 
     class Meta:
-        db_table    = 'posts_tags'
+        db_table = 'posts_tags'
