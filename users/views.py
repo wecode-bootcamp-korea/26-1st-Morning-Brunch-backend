9c559753c78172bbaad810dae52f28115e1f0482
YUBYUNGMOON <qudans1237@gmail.com>
2021-11-03 19:07:24 +0900

    [Morning Brunch]: singinu

--- users/views.py
+++ users/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+import json, re, bcrypt, jwt
+from os import name
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+from django.conf      import settings
+
+from .models          import User
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            email    = data['email']
+            password = data['password']
+            author_name = data['author_name']
+            author_job = data['author_job']
+            author_intro = data['author_intro']
+            
+            if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
+                return JsonResponse({"message": "INVALD_EMAIL"}, status=400)
+            
+            if not re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[?!@#$%*&])[A-Za-z\d?!@#$%*&]{8,}$', password):
+                return JsonResponse({"message": "INVALD_PASSWORD"}, status=400)
+            
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({"message": "EMAIL_EXISTS"}, status=400)
+
+            hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+            User.objects.create(
+                email = email,
+                password = hashed                
+            )
+            return JsonResponse({"message" : "SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"error_message" : "KEY_ERROR"}, status=400)
+
+class SignInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user = User.objects.get(email=data['email'])
+
+            if not User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message':'USER_DOES_NOT_EXIST'}, status=404)
+
+            if not bcrypt.checkpw(data["password"].encode("utf-8"), user.password.encode('utf-8')):
+                return JsonResponse({"MESSAGE":"INVALID_USER"}, status=401)
+
+            access_token = jwt.encode({"id" : user.id}, settings.SECRET_KEY, algorithm = settings.ALGORITHM)
+
+            return JsonResponse({"MESSAGE":"SUCCESS", "ACCESS_TOKEN": access_token}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+        
