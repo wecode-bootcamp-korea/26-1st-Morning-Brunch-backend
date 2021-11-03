248feee389448a6722bfcc60c564422997c5338c
YUBYUNGMOON <qudans1237@gmail.com>
2021-11-03 18:01:54 +0900

    [Morning Brunch]: signup

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json, re, bcrypt, jwt
+from os import name
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+from django.conf      import settings
+
+from .models          import User
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            email    = data['email']
+            password = data['password']
+
+            if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
+                return JsonResponse({"message" : "INVALD_EMAIL"}, status=400)
+            
+            if not re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[?!@#$%*&])[A-Za-z\d?!@#$%*&]{8,}$', password):
+                return JsonResponse({"message" : "INVALD_PASSWORD"}, status=400)
+        
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({"message" : "EMAIL_EXISTS"}, status=400)
+
+            hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+            User.objects.create(
+                email = email,
+                password = hashed_password
+            )        
+            return JsonResponse({"message" : "SUCCESS"}, status=201)
+       
+        except KeyError:
+            return JsonResponse({"massage" : "KEY_ERROR"}, status=400)
