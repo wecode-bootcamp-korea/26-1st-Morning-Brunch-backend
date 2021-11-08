30193ab464487c4a11993df1430ebbb514fea8cd
gshduet <modis2199@gmail.com>
2021-11-08 09:34:22 +0900

    ADD : Signin

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 import json, re, bcrypt, jwt
 
-from django.http      import JsonResponse
-from django.views     import View
-from django.conf      import settings
+from django.http    import JsonResponse
+from django.views   import View
 
-from .models          import User
+from MorningBrunch.settings    import SECRET_KEY, ALGORITHM
+from users.models   import User
 
 class SignUpView(View):
     def post(self, request):
@@ -17,15 +17,15 @@ class SignUpView(View):
             author_intro = data['author_intro']
 
             if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
-                return JsonResponse({"message" : "INVALD_EMAIL"}, status=400)
+                return JsonResponse({'message' : 'INVALD_EMAIL'}, status=400)
             
             if not re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[?!@#$%*&])[A-Za-z\d?!@#$%*&]{8,}$', password):
-                return JsonResponse({"message" : "INVALD_PASSWORD"}, status=400)
+                return JsonResponse({'message' : 'INVALD_PASSWORD'}, status=400)
         
             if User.objects.filter(email=email).exists():
-                return JsonResponse({"message" : "EMAIL_EXISTS"}, status=400)
+                return JsonResponse({'message' : 'EMAIL_EXISTS'}, status=400)
 
-            hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
             User.objects.create(
                 email       = email,
@@ -34,7 +34,27 @@ class SignUpView(View):
                 author_job  = author_job,
                 author_intro= author_intro,
             )        
-            return JsonResponse({"message" : "SUCCESS"}, status=201)
+            return JsonResponse({'message' : 'SUCCESS'}, status=201)
        
         except KeyError:
-            return JsonResponse({"massage" : "KEY_ERROR"}, status=400)
+            return JsonResponse({'massage' : 'KEY_ERROR'}, status=400)
+
+class SignInView(View):
+    def post(self, request):
+        try:
+            data        = json.loads(request.body)
+            email       = data['email']
+            password    = data['password']
+            user   = User.objects.get(email=data['email'])
+
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
+            access_token = jwt.encode({'id' : user.id}, SECRET_KEY, algorithm = ALGORITHM)
+            return JsonResponse({'MESSAGE':'SUCCESS', 'ACCESS_TOKEN': access_token}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message':'USER_DOES_NOT_EXIST'}, status=404)
