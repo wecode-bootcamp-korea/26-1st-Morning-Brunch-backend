8cb1d224a8b1099eff84ad2158dbcbae7abdf252
gshduet <modis2199@gmail.com>
2021-11-10 20:40:21 +0900

    FIX : offset,limit query parameter accept

--- posts/views.py
+++ posts/views.py
@@ -4,12 +4,14 @@ from django.views   import View
 from posts.models   import Post
 
 class PostList(View):
-    def get(self, request, offset, limit):
-        posts = Post.objects.all()[offset:limit]
-        result = [{
-            'post_id' : post.id,
-            'title' : post.title,
-            'author_name' : post.user.author_name,
-            'cover_image' : post.coverimage_set.first().url
+    def get(self, request):
+        offset  = int(request.GET.get('offset',0))
+        limit   = int(request.GET.get('limit',15))
+        posts   = Post.objects.all()[offset:limit]
+        result  = [{
+            'post_id'       : post.id,
+            'title'         : post.title,
+            'author_name'   : post.user.author_name,
+            'cover_image'   : post.coverimage_set.first().url
             } for post in posts]
         return JsonResponse({'liked_contents_data':result}, status = 200)
