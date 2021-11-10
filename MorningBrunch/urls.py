63190a431ec4a7890b51112eea4e0592da3d0a25
YUBYUNGMOON <qudans1237@gmail.com>
2021-11-10 18:08:49 +0900

    ADD : POSTVIEW

--- MorningBrunch/urls.py
+++ MorningBrunch/urls.py
@@ -17,5 +17,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users', include('users.urls')),
+    path('posts', include('posts.urls')),
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -9,22 +9,17 @@ class PostView(View):
             post = Post.objects.select_related('user').get(id = post_id)
             
             result = {          
-                # "cover_image" : post.coverimage_set.url,
+                "cover_image" : post.coverimage_set.all().values('url'),
                 "title"       : post.title,
                 "sub_title"   : post.sub_title,
                 "creat_time"  : post.create_time,
                 "content"     : post.content,
                 "tags"        : [tag.name for tag in post.tags.all()],
-                                #포스트(get post_id)>태그>post_id 태그갖고오기
                 "author" : {
                     "name"          : post.user.author_name,
-                                    #포스트(get post_id)>user>author_name 갖고오기
                     "introduction"  : post.user.author_intro,
-                    "user_tag"      : [tag.name for tag in post.tags.all()],
-                                     #포스트(get post_id)>tag>user_id>태그갖고오기 / 유저아이디로 다시 필터링?
-                                     #포스트(get post_id) > user > tag > user tag 갖고오기 / 유저아이디로 다시 필터링?
-                    "profile_image" : post.user.userimage_set.filter(id=post_id).values('url')
-                                    # id =2은 결과가 잘나오지만 나머지 id 1,3,4,5는 <QuerySet []> 으로 나타남
+                    "user_tag"      : [tag.name for tag in post.user.tags.all()],
+                    "profile_image" : post.user.userimage_set.all().values('url')
                     }
                 }
 
