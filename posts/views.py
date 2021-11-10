e8283e0477e3b22e01dd787d2143f92d0c8c0632
gshduet <modis2199@gmail.com>
2021-11-10 13:46:31 +0900

    Pair Coding'

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.http    import JsonResponse
+from django.views   import View
+
+from users.models   import Tag
+from posts.models   import Post
+
+class LikeContentView(View):
+    def get(self, request):
+        try:
+            posts = Post.objects.all()
+            result = [
+                {
+                'post_id' : post.id,
+                'title' : post.title,
+                'author_name' : post.user.author_name,
+                'cover_image' : post.coverimage_set.values('url')[0].get('url')}
+                for post in posts]
+            return JsonResponse({'liked_contents_data':result}, status = 200)
+
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+
+class PostKeyWordView(View):
+    def get(self,request):
+        try:
+            tags = Tag.objects.all()
+            posts_tags = []
+
+            for tag in tags:
+                if len(tag.posts.values('title')) != 0 :
+                    posts_tags.append(tag.name)
+
+            result = random.sample(posts_tags,24)
+            return JsonResponse({"keyword_data":result}, status = 200)
+        
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400) 
