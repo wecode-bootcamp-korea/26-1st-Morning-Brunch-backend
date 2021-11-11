d0dbca5cb955b5150646fb28de54cc0ce5ff8a08
YUBYUNGMOON <qudans1237@gmail.com>
2021-11-11 20:48:48 +0900

    FIX : CONFLICT ERROR

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from posts.views import PostView, PostList, TagsView, UsersView
+from posts.views import PostView, PostList, TagsView, UserView, UsersView
 
 urlpatterns = [
     path("/post/<int:post_id>", PostView.as_view()),
     path('postlist', PostList.as_view()),
     path('/keyword', TagsView.as_view()),
-    path("/users/<int:user_id>", UsersView.as_view()),
+    path("/user/<int:user_id>", UserView.as_view()),
+    path("/users/<int:user_id>", UsersView.as_view())
 ]
