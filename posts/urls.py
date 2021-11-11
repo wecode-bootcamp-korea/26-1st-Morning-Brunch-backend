71be97e877bc3a5d43601e08b297c512e9c5b0db
YUBYUNGMOON <qudans1237@gmail.com>
2021-11-11 18:46:15 +0900

    ADD : UserView

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from posts.views import PostView, PostList, TagsView
+from posts.views import PostView, PostList, TagsView, UserView
 
 urlpatterns = [
     path("/<int:post_id>", PostView.as_view()),
     path('postlist', PostList.as_view()),
     path('/keyword', TagsView.as_view()),
+    path("/user/<int:user_id>", UserView.as_view()),
 ]
