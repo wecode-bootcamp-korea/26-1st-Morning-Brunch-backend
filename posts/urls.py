be5cecfa6dc026d198f477973d4e73190f122ae9
gshduet <modis2199@gmail.com>
2021-11-11 23:20:01 +0900

    FIX : url 이탈자 수정

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from posts.views import PostView, PostList, TagsView, UserView, UsersView, Autho
 
 urlpatterns = [
     path('/post/<int:post_id>', PostView.as_view()),
-    path('postlist', PostList.as_view()),
+    path('/postlist', PostList.as_view()),
     path('/keyword', TagsView.as_view()),
     path('/user/<int:user_id>', UserView.as_view()),
     path('/users/<int:user_id>', UsersView.as_view()),
