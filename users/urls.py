27101b864f4eaeb99df8c4331f65899031da08fe
gshduet <modis2199@gmail.com>
2021-11-12 15:36:31 +0900

    FIX : api directory relocate

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from users.views      import SignUpView, SignInView
+from users.views      import SignUpView, SignInView, UserView, UsersView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
+    path('/user/<int:user_id>', UserView.as_view()),
+    path('/users/<int:user_id>', UsersView.as_view()),
 ]
